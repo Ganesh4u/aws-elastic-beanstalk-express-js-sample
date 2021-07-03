01383d2d689f52a8ac019d4a96288a0a4399afbd
Ganesh4u <gkapote@gmail.com>
2021-07-04 01:26:42 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express();
 const port = 8080;
 
 app.get('/', (req, res) => res.send('Hello World by Ganesh K from Github!!!'));
+app.get('/', (req, res) => res.send('Hello World by Ganesh K from Github!!!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
